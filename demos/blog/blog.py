76f87efb90e462cf15dbc008cdd8ac035038c3d9
unknown <Navneet@.(none)>
2011-08-03 22:53:24 +0530

    OOK

--- demos/blog/blog.py
+++ demos/blog/blog.py
@@ -26,7 +26,7 @@ import tornado.web
 import unicodedata
 
 from tornado.options import define, options
-
+#"It is not working "
 define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
 define("mysql_database", default="blog", help="blog database name")
